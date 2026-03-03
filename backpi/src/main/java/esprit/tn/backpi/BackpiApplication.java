b5d92ecb88e37134871990290a9a0efc8c17deef
moohaammed <130971831+moohaammed@users.noreply.github.com>
2026-03-03 11:00:25 +0100

    firstpush

---
+++ backpi/src/main/java/esprit/tn/backpi/BackpiApplication.java
@@ -0,0 +1,13 @@
+package esprit.tn.backpi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackpiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackpiApplication.class, args);
+    }
+
+}
---
+++ backpi/src/test/java/esprit/tn/backpi/BackpiApplicationTests.java
@@ -0,0 +1,13 @@
+package esprit.tn.backpi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackpiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
